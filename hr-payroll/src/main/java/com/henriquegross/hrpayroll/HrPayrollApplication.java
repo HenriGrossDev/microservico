d2635f35d68d0d3a67f25002faa4d9706569d71e
Henrique Grosscklauss <henriquegrossdeveloper@gmail.com>
2022-11-09 16:00:07 -0300

    Rest Template

--- hr-payroll/src/main/java/com/henriquegross/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/henriquegross/hrpayroll/HrPayrollApplication.java
@@ -2,8 +2,13 @@ package com.henriquegross.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.henriquegross.hrpayroll.*")
+@EntityScan("com.henriquegross.payroll.entities")
+
 public class HrPayrollApplication {
 
 	public static void main(String[] args) {
